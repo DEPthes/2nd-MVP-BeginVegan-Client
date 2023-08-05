d841e4533a95270d0b2fad8a112d572eff9b0fa7
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-08-05 16:33:11 +0900

    [chore] 카카오맵 API 앱 등록 및 해시키 등록

--- app/src/main/java/com/example/beginvegan/src/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.beginvegan.src.ui.view
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.beginvegan.R
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "SOL_LOG"
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            val information = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            val signatures = information.signingInfo.apkContentsSigners
+            for (signature in signatures) {
+                val md = MessageDigest.getInstance("SHA").apply {
+                    update(signature.toByteArray())
+                }
+                val HASH_CODE = String(Base64.encode(md.digest(), 0))
+
+                Log.d(TAG, "HASH_CODE -> $HASH_CODE")
+            }
+        } catch (e: Exception) {
+            Log.d(TAG, "Exception -> $e")
+        }
     }
 }
