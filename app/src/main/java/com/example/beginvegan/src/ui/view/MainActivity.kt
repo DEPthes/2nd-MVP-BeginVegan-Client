5597c56608165e9049951207988a1d3daea19822
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-08-05 16:34:01 +0900

    [fix] main 코드 수정

--- app/src/main/java/com/example/beginvegan/src/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/MainActivity.kt
@@ -1,36 +1,17 @@
 package com.example.beginvegan.src.ui.view
 
-import android.content.pm.PackageManager
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
-import androidx.annotation.RequiresApi
 import com.example.beginvegan.R
-import java.security.MessageDigest
+import com.example.beginvegan.config.BaseActivity
+import com.example.beginvegan.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private val TAG = "SOL_LOG"
-
-    @RequiresApi(Build.VERSION_CODES.P)
+class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.text
+    }
+    override fun init() {
 
-        try {
-            val information = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
-            val signatures = information.signingInfo.apkContentsSigners
-            for (signature in signatures) {
-                val md = MessageDigest.getInstance("SHA").apply {
-                    update(signature.toByteArray())
-                }
-                val HASH_CODE = String(Base64.encode(md.digest(), 0))
-
-                Log.d(TAG, "HASH_CODE -> $HASH_CODE")
-            }
-        } catch (e: Exception) {
-            Log.d(TAG, "Exception -> $e")
-        }
     }
 }
