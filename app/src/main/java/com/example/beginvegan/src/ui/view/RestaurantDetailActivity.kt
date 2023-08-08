c90b36f843648e9c2d38ef5bff96ca96b3facb04
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-08 22:37:11 +0900

    [design] main UI 추가
    bottom nav에 들어가는 home, recipe, profile 구현

---
+++ app/src/main/java/com/example/beginvegan/src/ui/view/RestaurantDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beginvegan.src.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beginvegan.R
+
+class RestaurantDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_detail)
+    }
+}
