db0dee91163f8654b512b484f17f8c80b35da9e2
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-08 19:09:38 +0900

    [design] login and test UI 추가

---
+++ app/src/main/java/com/example/beginvegan/src/ui/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beginvegan.src.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beginvegan.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/beginvegan/src/ui/view/VeganTestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beginvegan.src.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beginvegan.R
+
+class VeganTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vegan_test)
+    }
+}
