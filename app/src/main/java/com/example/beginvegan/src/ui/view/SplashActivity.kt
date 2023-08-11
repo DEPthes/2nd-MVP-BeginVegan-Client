104378702c00fb09c2c83997989292c9667e7c74
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-11 20:32:02 +0900

    [feat] splash screen 추가
    화면 추가 및 실행 시 띄우는 것까지 추가했습니다.

---
+++ app/src/main/java/com/example/beginvegan/src/ui/view/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.beginvegan.src.ui.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beginvegan.R
+
+class  SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+
+        finish( )
+    }
+}
