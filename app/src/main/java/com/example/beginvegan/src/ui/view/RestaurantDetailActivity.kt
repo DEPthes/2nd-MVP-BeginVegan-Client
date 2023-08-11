111aadc4dab2127b24704c8f813ee449dd3fd21f
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-11 23:30:53 +0900

    [docs] BaseActivity 연결

--- app/src/main/java/com/example/beginvegan/src/ui/view/RestaurantDetailActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/RestaurantDetailActivity.kt
@@ -3,10 +3,16 @@ package com.example.beginvegan.src.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.beginvegan.R
+import com.example.beginvegan.config.BaseActivity
+import com.example.beginvegan.databinding.ActivityLoginBinding
+import com.example.beginvegan.databinding.ActivityRestaurantDetailBinding
 
-class RestaurantDetailActivity : AppCompatActivity() {
+class RestaurantDetailActivity : BaseActivity<ActivityRestaurantDetailBinding>({ ActivityRestaurantDetailBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_detail)
+    }
+    override fun init() {
+
     }
 }
--- app/src/main/java/com/example/beginvegan/src/ui/view/WriteReviewActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/WriteReviewActivity.kt
@@ -3,10 +3,16 @@ package com.example.beginvegan.src.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.beginvegan.R
+import com.example.beginvegan.config.BaseActivity
+import com.example.beginvegan.databinding.ActivityRestaurantDetailBinding
+import com.example.beginvegan.databinding.ActivityWriteReviewBinding
 
-class WriteReviewActivity : AppCompatActivity() {
+class WriteReviewActivity : BaseActivity<ActivityWriteReviewBinding>({ ActivityWriteReviewBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_review)
+    }
+    override fun init() {
+
     }
 }
