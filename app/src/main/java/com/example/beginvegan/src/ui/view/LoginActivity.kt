c99d9131bb353133e5d0bc9074c034e9ce4b4023
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-11 22:01:09 +0900

    [docs] status bar 색상 수정
    color - primary1, 2, 3으로 수정 및 추가

--- app/src/main/java/com/example/beginvegan/src/ui/view/LoginActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.beginvegan.src.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.beginvegan.R
+import com.example.beginvegan.config.BaseActivity
+import com.example.beginvegan.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>({ ActivityLoginBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+    }
+
+    override fun init() {
+
     }
 }
