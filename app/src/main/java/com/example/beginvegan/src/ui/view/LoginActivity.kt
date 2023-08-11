7559251843d2f037eb0a063fce7f9dee1453278d
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-08-12 04:05:13 +0900

    [feat] 로그인 이후 Splash 적용

--- app/src/main/java/com/example/beginvegan/src/ui/view/LoginActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/LoginActivity.kt
@@ -1,6 +1,6 @@
 package com.example.beginvegan.src.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.example.beginvegan.config.BaseActivity
@@ -23,33 +23,39 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>({ActivityLoginBinding.i
         }
     }
     override fun init() {
+        binding.btnLoginKakao.setOnClickListener {
+//            kakaoLogin()
+            TestLogin()
+        }
+    }
+    private fun TestLogin(){
+        val intent = Intent(this,WelcomeActivity::class.java)
+        startActivity(intent)
+    }
+    private fun kakaoLogin(){
         KakaoSdk.init(this, "fb5550ba69a57eb73aae78bc996d8a8e")
-        binding.btnLoginKakao.setOnClickListener{
-            // 이메일 로그인 콜백
-// 카카오톡 설치 확인
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-                // 카카오톡 로그인
-                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                    // 로그인 실패 부분
-                    if (error != null) {
-                        Log.e("KaKao Login", "로그인 실패 $error")
-                        // 사용자가 취소
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
-                            return@loginWithKakaoTalk
-                        }
-                        // 다른 오류
-                        else {
-                            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
-                        }
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            // 카카오톡 로그인
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                // 로그인 실패 부분
+                if (error != null) {
+                    Log.e("KaKao Login", "로그인 실패 $error")
+                    // 사용자가 취소
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
+                        return@loginWithKakaoTalk
                     }
-                    // 로그인 성공 부분
-                    else if (token != null) {
-                        Log.e("KaKao Login", "로그인 성공 ${token.accessToken}")
+                    // 다른 오류
+                    else {
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
                     }
                 }
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
+                // 로그인 성공 부분
+                else if (token != null) {
+                    Log.e("KaKao Login", "로그인 성공 ${token.accessToken}")
+                }
             }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
         }
     }
 }
