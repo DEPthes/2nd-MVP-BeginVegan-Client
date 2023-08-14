3df7d1c61a66d63e4c70615a32284f226a50039e
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-14 21:25:49 +0900

    [docs] magazine fragment 추가

---
+++ app/src/main/java/com/example/beginvegan/src/ui/view/HomeMagazineVeganTypesFragment.kt
@@ -0,0 +1,18 @@
+package com.example.beginvegan.src.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.beginvegan.R
+
+class HomeMagazineVeganTypesFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
