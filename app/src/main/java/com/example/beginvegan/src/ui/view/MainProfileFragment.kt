9dcbce944547fa5235022c364dac96e4bc648709
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-19 04:24:16 +0900

    [feat] TabLayout, ViewPager 연결 수정 완료

--- app/src/main/java/com/example/beginvegan/src/ui/view/MainProfileFragment.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/MainProfileFragment.kt
@@ -1,11 +1,17 @@
 package com.example.beginvegan.src.ui.view
 
 import android.app.AlertDialog
+import android.text.Layout
 import android.util.Log
+import android.widget.TableLayout
+import androidx.fragment.app.Fragment
 import com.example.beginvegan.R
 import com.example.beginvegan.config.BaseFragment
 import com.example.beginvegan.databinding.FragmentMainProfileBinding
 import com.example.beginvegan.src.ui.adapter.ProfileMyRecordsVPAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainProfileFragment : BaseFragment<FragmentMainProfileBinding>(
     FragmentMainProfileBinding::bind, R.layout.fragment_main_profile
@@ -14,15 +20,45 @@ class MainProfileFragment : BaseFragment<FragmentMainProfileBinding>(
 
         Log.d("TAG", "init: mainProfile")
 
-
         //ViewPager
         val vpMyRecords = binding.vpMyRecords
-        val tabLayout = binding.tlRecords
-//        val profileMyRecordsVPAdapter = ProfileMyRecordsVPAdapter(this)
         vpMyRecords.adapter = ProfileMyRecordsVPAdapter(this)
 
+        //tab
+        Log.d("TAG", "init: mainProfile")
+        //ViewPager
+        val tabLayout = binding.tlRecords
+        TabLayoutMediator(tabLayout,vpMyRecords){
+                tab, pos ->
+            when(pos){
+                0-> tab.text = "나의 리뷰"
+                1-> tab.text = "나의 스크랩"
+            }
+        }.attach()
+//        requireActivity().supportFragmentManager.beginTransaction().add(R.id.vp_my_records, ProfileMyreviewFragment()).commit()
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                when(tab?.position){
+//                    0 ->{replaceView(ProfileMyreviewFragment())}
+//                    1 ->{replaceView(ProfileMyscrapFragment())}
+//                }
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {}
+//        })
+//        tabLayout.addOnTabSelectedListener(object : TableLayout.OnTabSelectedListener{
+//            override fun OnTabSelectedListener(tab: Layout.Tab?)
+//
+//        })
         //닉네임 수정 dialog
 
     }
 
+    private fun replaceView(tab: Fragment){
+        tab.let{
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.vp_my_records, it).commit()
+        }
+    }
 }
