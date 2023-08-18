f88f3261b0e82149a3c8d2c62a444f32180abb15
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-08-19 07:12:01 +0900

    [feat] Kakao MapView Marker Pin 추가 및 ClickListner 추가

--- app/src/main/java/com/example/beginvegan/src/ui/view/VeganMapFragment.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/VeganMapFragment.kt
@@ -16,24 +16,30 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.beginvegan.R
 import com.example.beginvegan.config.BaseFragment
 import com.example.beginvegan.databinding.FragmentVeganMapBinding
 import com.example.beginvegan.src.ui.adapter.VeganMapBottomSheetRVAdapter
 import com.example.beginvegan.util.Constants.ACCESS_FINE_LOCATION
+import com.example.beginvegan.util.MarkerEventListener
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+
 
 class VeganMapFragment : BaseFragment<FragmentVeganMapBinding>(
     FragmentVeganMapBinding::bind,
     R.layout.fragment_vegan_map
-) {
+),MapView.POIItemEventListener
+{
     private lateinit var dataList: ArrayList<String>
     private lateinit var mapView: MapView
+    private lateinit var markerEventListener: MarkerEventListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -66,6 +72,18 @@ class VeganMapFragment : BaseFragment<FragmentVeganMapBinding>(
             add("hello2")
             add("hello3")
             add("hello4")
+            add("hello1")
+            add("hello2")
+            add("hello3")
+            add("hello4")
+            add("hello1")
+            add("hello2")
+            add("hello3")
+            add("hello4")
+            add("hello1")
+            add("hello2")
+            add("hello3")
+            add("hello4")
         }
         val dataRVAdapter  = VeganMapBottomSheetRVAdapter(dataList)
         binding.veganmapBottomSheet.rvBottomSheetRestaurantList.adapter = dataRVAdapter
@@ -82,6 +100,42 @@ class VeganMapFragment : BaseFragment<FragmentVeganMapBinding>(
             }
 
         })
+    }
+    private fun checkLocationService(): Boolean {
+        val locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+    private fun setMapView(){
+        mapView = MapView(this@VeganMapFragment.activity)
+        binding.mvVeganMap.addView(mapView)
+    }
+    private fun startTracking(){
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+        dismissLoadingDialog()
+        setRestaurantGps()
+    }
+
+    private fun setRestaurantDetail(){
+        // 주변식당 조회시 리사이클러뷰에 등록
+    }
+    private fun setRestaurantGps(){
+        markerEventListener = MarkerEventListener(requireContext())
+        // for문으로 데이터 갯수만큼 돌리기
+        var la = 37.223036
+        var lo = 127.187954
+        for(i: Int in 1..10){
+            val marker = MapPOIItem()
+            la -= 0.0001
+            lo += 0.0001
+            marker.itemName = "Default$i"
+            marker.tag = 0
+            marker.mapPoint = MapPoint.mapPointWithGeoCoord(la,lo)
+            marker.markerType = MapPOIItem.MarkerType.BluePin // 기본으로 제공하는 BluePin 마커 모양.
+            marker.selectedMarkerType =
+                MapPOIItem.MarkerType.RedPin // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+            mapView.addPOIItem(marker)
+        }
+        mapView.setPOIItemEventListener(this)
     }
 
     // 권한에 대한 메소드
@@ -143,26 +197,38 @@ class VeganMapFragment : BaseFragment<FragmentVeganMapBinding>(
             }
         }
     }
-
-    // GPS가 켜져있는지 확인
-    private fun checkLocationService(): Boolean {
-        val locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-    }
-    private fun setMapView(){
-        mapView = MapView(this@VeganMapFragment.activity)
-        binding.mvVeganMap.addView(mapView)
-    }
-    private fun startTracking(){
-        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-        dismissLoadingDialog()
-    }
-
     override fun onPause() {
         super.onPause()
         binding.mvVeganMap.removeAllViews()
     }
     override fun onDestroyView() {
         super.onDestroyView()
+    }
+
+    override fun onPOIItemSelected(p0: MapView?, p1: MapPOIItem?) {
+        Toast.makeText(context, p1!!.itemName.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {
+    }
+
+    override fun onCalloutBalloonOfPOIItemTouched(
+        mapView: MapView?, poiItem: MapPOIItem?, buttonType: MapPOIItem.CalloutBalloonButtonType?
+    ) {
+        // 말풍선 클릭 시
+        val builder = android.app.AlertDialog.Builder(context)
+        val itemList = arrayOf("토스트", "마커 삭제", "취소")
+        builder.setTitle("${poiItem?.itemName}")
+        builder.setItems(itemList) { dialog, which ->
+            when(which) {
+                0 -> Toast.makeText(context, "토스트", Toast.LENGTH_SHORT).show()  // 토스트
+                1 -> mapView?.removePOIItem(poiItem)    // 마커 삭제
+                2 -> dialog.dismiss()   // 대화상자 닫기
+            }
+        }
+        builder.show()
+    }
+
+    override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {
     }
 }
