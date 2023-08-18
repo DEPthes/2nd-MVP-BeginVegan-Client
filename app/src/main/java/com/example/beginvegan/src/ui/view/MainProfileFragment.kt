fcfa32a6837268293a8e4473843eb42f05f9b49f
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-08-19 05:29:04 +0900

    [feat] 닉네임 수정 dialog 추가

--- app/src/main/java/com/example/beginvegan/src/ui/view/MainProfileFragment.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/MainProfileFragment.kt
@@ -17,16 +17,10 @@ class MainProfileFragment : BaseFragment<FragmentMainProfileBinding>(
     FragmentMainProfileBinding::bind, R.layout.fragment_main_profile
 ) {
     override fun init() {
-
-        Log.d("TAG", "init: mainProfile")
-
         //ViewPager
         val vpMyRecords = binding.vpMyRecords
         vpMyRecords.adapter = ProfileMyRecordsVPAdapter(this)
-
         //tab
-        Log.d("TAG", "init: mainProfile")
-        //ViewPager
         val tabLayout = binding.tlRecords
         TabLayoutMediator(tabLayout,vpMyRecords){
                 tab, pos ->
@@ -35,30 +29,16 @@ class MainProfileFragment : BaseFragment<FragmentMainProfileBinding>(
                 1-> tab.text = "나의 스크랩"
             }
         }.attach()
-//        requireActivity().supportFragmentManager.beginTransaction().add(R.id.vp_my_records, ProfileMyreviewFragment()).commit()
-//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-//            override fun onTabSelected(tab: TabLayout.Tab?) {
-//                when(tab?.position){
-//                    0 ->{replaceView(ProfileMyreviewFragment())}
-//                    1 ->{replaceView(ProfileMyscrapFragment())}
-//                }
-//            }
-//
-//            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-//
-//            override fun onTabReselected(tab: TabLayout.Tab?) {}
-//        })
-//        tabLayout.addOnTabSelectedListener(object : TableLayout.OnTabSelectedListener{
-//            override fun OnTabSelectedListener(tab: Layout.Tab?)
-//
-//        })
-        //닉네임 수정 dialog
 
+        //닉네임 수정 dialog
+        binding.ibEditUsername.setOnClickListener{
+            openEditUserNameDialog()
+        }
     }
 
-    private fun replaceView(tab: Fragment){
-        tab.let{
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.vp_my_records, it).commit()
-        }
+    //recipe Dialog
+    fun openEditUserNameDialog(){
+        val dialog = ProfileEditNameDialog(requireContext())
+        dialog.show()
     }
 }
