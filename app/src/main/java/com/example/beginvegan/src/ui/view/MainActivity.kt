23b1b4062eccb03e40b03e9be1d1303b0fc964cc
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-08-21 01:45:55 +0900

    [feat] GPS기능 추가 및 권한설정 변경

--- app/src/main/java/com/example/beginvegan/src/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/view/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.beginvegan.src.ui.view
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.beginvegan.R
+import com.example.beginvegan.config.ApplicationClass
 import com.example.beginvegan.config.BaseActivity
 import com.example.beginvegan.databinding.ActivityMainBinding
 import com.example.beginvegan.src.data.model.restaurant.Coordinate
 import com.example.beginvegan.src.ui.view.mainhome.MainHomeFragment
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it)}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
     override fun init() {
+        getLocation()
         supportFragmentManager.beginTransaction().replace(R.id.fl_main,MainHomeFragment()).commit()
 
         binding.bnvMain.setOnItemSelectedListener {
@@ -44,5 +51,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
 
         supportFragmentManager.beginTransaction().replace(R.id.fl_main, fragment).commit()
         binding.bnvMain.selectedItemId = R.id.item_recipe
+    }
+    @SuppressLint("MissingPermission")
+    private fun getLocation(){
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+
+        fusedLocationProviderClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY,null)
+            .addOnSuccessListener { success: Location? ->
+                success?.let { location ->
+                    ApplicationClass.xLatitude = (location.latitude).toString()
+                    ApplicationClass.xLongitude = (location.longitude).toString()
+                }
+            }
+            .addOnFailureListener { fail ->
+                Toast.makeText(this,fail.message,Toast.LENGTH_SHORT).show()
+            }
     }
 }
