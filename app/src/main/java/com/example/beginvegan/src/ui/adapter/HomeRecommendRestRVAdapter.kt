753096bbee0a9a807671ccaf9698577178f919a9
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-09-09 14:46:28 +0900

    [fix] 메인 홈 추천 식당 Adapter 수정

--- app/src/main/java/com/example/beginvegan/src/ui/adapter/HomeRecommendRestRVAdapter.kt
+++ app/src/main/java/com/example/beginvegan/src/ui/adapter/HomeRecommendRestRVAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -12,42 +13,56 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.beginvegan.R
 import com.example.beginvegan.databinding.ItemHomeRecommendRestaurantBinding
+import com.example.beginvegan.databinding.ItemVeganmapRestaurantBinding
 import com.example.beginvegan.src.data.model.restaurant.NearRestaurant
 
-class HomeRecommendRestRVAdapter(private val context: Context,private val recommendRestList: ArrayList<NearRestaurant>):
-    RecyclerView.Adapter<HomeRecommendRestRVAdapter.RecyclerViewHolder>() {
+class HomeRecommendRestRVAdapter(
+    private val context: Context,
+    private val recommendRestList: ArrayList<NearRestaurant>
+) :
+    RecyclerView.Adapter<HomeRecommendRestRVAdapter.DataViewHolder>() {
+    private var listener: HomeRecommendRestRVAdapter.OnItemClickListener? = null
+    inner class DataViewHolder(private val binding: ItemHomeRecommendRestaurantBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.tvRestaurantName.text = recommendRestList[position].name
+            if (!recommendRestList[position].imageUrl.isNullOrEmpty()) {
+                Glide.with(context).load(recommendRestList[position].imageUrl).transform(CenterCrop(), RoundedCorners(8))
+                    .into(binding.ivRestaurantImg)
+            } else {
+                Glide.with(context).load(R.drawable.test_res)
+                    .transform(CenterCrop(), RoundedCorners(8)).into(binding.ivRestaurantImg)
+            }
+        }
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HomeRecommendRestRVAdapter.DataViewHolder {
         val binding = ItemHomeRecommendRestaurantBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-        return RecyclerViewHolder(context, binding)
+        return DataViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        val item = recommendRestList[position % recommendRestList.size]
-        holder.bind(item)
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        holder.bind(position)
     }
 
-    override fun getItemCount() = Int.MAX_VALUE
+    override fun getItemCount(): Int = recommendRestList.size
 
-    class RecyclerViewHolder(private val context: Context, private val binding: ItemHomeRecommendRestaurantBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        val sampleImg = listOf(
-            R.drawable.test_res,
-            R.drawable.test_res2,
-            R.drawable.test_home_re3
-        )
-        fun bind(data:NearRestaurant) {
-            binding.tvRestaurantName.text = data.name
-            if(!data.imageUrl.isNullOrEmpty()){
-                Glide.with(context).load(data.imageUrl).transform(CenterCrop(),RoundedCorners(8)).into(binding.ivRestaurantImg)
-            }else{
-                Glide.with(context).load(sampleImg[data.id%3]).transform(CenterCrop(),RoundedCorners(8)).into(binding.ivRestaurantImg)
-                Log.d("TAG", "bind: ${data.id}")
-            }
-        }
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(v: View, data: NearRestaurant, position: Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
     }
 }
